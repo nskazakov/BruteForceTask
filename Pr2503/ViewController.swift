e4bc4ac2943f44e4a0bbfafbd3cf270c521fc550
Denis Aganov <den.aganov@yandex.ru>
2021-12-30 12:20:03 +0300

    Fix constraint. Add timer for update label password.

--- Pr2503/ViewController.swift
+++ Pr2503/ViewController.swift
@@ -18,34 +18,50 @@ class ViewController: UIViewController {
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     @IBOutlet weak var labelPassword: UILabel!
+    @IBOutlet weak var newPassword: UIButton!
+    
+    @IBAction func generatePassword(_ sender: Any) {
+        textField.text = String().newPassword(of: 3)
+    }
     
     @IBAction func startButton(_ sender: Any) {
-        let brutForce = BruteForce(of: 3)
-        
-        textField.isSecureTextEntry = true
-        textField.text = brutForce.password
-        
-        labelPassword.text = "Подбор пароля..."
+        var password = textField.text ?? ""
+        if password.isEmpty {
+            password = String().newPassword(of: 3)
+            textField.text = password
+        }
         
         (sender as? UIButton)?.isEnabled = false
+        newPassword.isEnabled = false
         
         activityIndicator.startAnimating()
         
-        let forcing = BruteForceOperation(brutForce)
+        let forcing = BruteForceOperation(password: password)
         forcing.completionBlock = {
             DispatchQueue.main.async {
                 self.activityIndicator.stopAnimating()
                 self.labelPassword.text = forcing.password
-                self.textField.isSecureTextEntry = false
                 (sender as? UIButton)?.isEnabled = true
+                self.newPassword.isEnabled = true
             }
         }
         bruteForceQueue.addOperation(forcing)
+        
+        let timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { timer in
+            self.labelPassword.text = forcing.password
+            print(forcing.password)
+            if !forcing.isExecuting {
+                timer.invalidate()
+            }
+        }
+        timer.fire()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        textField.isSecureTextEntry = true
         activityIndicator.hidesWhenStopped = true
     }
+    
 }
